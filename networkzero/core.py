4d59b49fdf7f43eb1b7aa6b2db52c77437b1db6a
Tim Golden <mail@timgolden.me.uk>
2016-04-04 09:59:04 +0100

    Slightly refactor core.address

--- networkzero/core.py
+++ networkzero/core.py
@@ -41,14 +41,15 @@ def address(address=None):
     """Convert one of a number of inputs into a valid ip:port string.
     
     Elements which are not provided are filled in as follows:
-    * IP Address: the system is asked for the set of IP addresses associated 
-      with the machine and the first one is used.
-    * Port number: a random port is selected from the pool of dynamically-available 
-      port numbers.
+        
+        * IP Address: the system is asked for the set of IP addresses associated 
+          with the machine and the first one is used.
+        * Port number: a random port is selected from the pool of dynamically-available 
+          port numbers.
       
     This means you can pass any of: nothing; an IP address; a port number; both
     
-    If an IP address is supplied but is invalid from this machine, an InvalidAddressError
+    If an IP address is supplied but is invalid, an InvalidAddressError
     exception is raised.
     
     :param address: (optional) Any of: an IP address, a port number, or both
@@ -56,6 +57,11 @@ def address(address=None):
     :returns: a valid ip:port string for this machine
     """
     address = str(address or "").strip()
+    #
+    # If the address is an ip:port pair, split into its component parts.
+    # Otherwise, try to determine whether we're looking at an IP
+    # or at a port and leave the other one blank
+    #
     if ":" in address:
         ip, _, port = address.partition(":")
     else:   
@@ -64,22 +70,38 @@ def address(address=None):
         else:
             ip, port = address, ""
     
-    if port and not port.isdigit():
-        raise InvalidAddressError("Port %s must be a number" % port)
-    
-    if not port or not int(port):
+    #
+    # If the port has been supplied, make sure it's numeric and that it's a valid
+    # port number. If it hasn't been supplied, remove a random one from the pool
+    # of possible dynamically-allocated ports and use that.
+    #
+    if port:
+        try:
+            port = int(port)
+        except ValueError:
+            raise InvalidAddressError("Port %s must be a number" % port)
+        if port not in config.VALID_PORTS:
+            raise InvalidAddressError("Port %d must be in range %d - %d" % (
+                port, config.VALID_PORTS.start, config.VALID_PORTS.stop)
+            )
+    else:
         random.shuffle(PORT_POOL)
         port = PORT_POOL.pop()
-    
-    if not int(port) in config.VALID_PORTS:
-        raise InvalidAddressError("Invalid port %s" % port)
-    
+
+    #
+    # Attempt to valid the ip:port pair as a valid INET address.
+    # If the IP address has not been supplied, this will return possible matches
+    # from the list of IP addresses associated with this machine
+    #
     try:
         addrinfo = socket.getaddrinfo(ip, port, socket.AF_INET)
     except socket.gaierror as exception:
         _logger.exception("Invalid Address %s", address)
         raise InvalidAddressError("Invalid IP %s" % ip)
     
+    #
+    # Pick an arbitrary one of the IP addresses matching the address
+    #
     for addrinfo in addrinfo:
         return "%s:%s" % addrinfo[-1]
     else:
