873b7b1f885a93eba8537e140c5ba55d0d12bbb1
Tim Golden <mail@timgolden.me.uk>
2016-04-04 09:06:01 +0100

    Tidying up after the logging refactor

--- shell.py
+++ shell.py
@@ -5,9 +5,11 @@ import networkzero as nw0
 import logging
 logger = logging.getLogger("networkzero")
 handler = logging.FileHandler("network.log", encoding="utf-8")
-handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+handler.setFormatter(logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s"))
+handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+handler.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
+handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 del handler, logging
