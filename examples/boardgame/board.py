aec2ff41c7ff8ebc98f89cbfbdb4f62468dc5dd7
Tim Golden <mail@timgolden.me.uk>
2016-04-05 13:50:58 +0100

    Update the skeleton board game to use a notification framework

--- examples/boardgame/board.py
+++ examples/boardgame/board.py
@@ -1,25 +1,33 @@
+import time
 import networkzero as nw0
 
+notifications = nw0.advertise("board-updates")
 address = nw0.advertise("board")
+
 board = {}
 while True:
     message = nw0.wait_for_message(address)
-    #
-    # Receive a move from a player
-    #
-    player, move = message
-    print("Player %s has sent move %s" % (player, move))
+    segments = message.lower().split()
+    command, params = segments[0], segments[1:]
+    print("Command %s with params %s" % (command, params))
     
     #
-    # Update the board if a move was made. (This allows players to request
-    # the current state of the board by making an empty move).
+    # Depending on what the command is, update the board or do whatever
+    # is needed. 
     #
-    if move:
-        #
-        # Do something with the board according to this move, eg:
-        #
+    if command == "move":
+        [player] = params
         board[move] = player
+    elif command == "restart":
+        board.clear()
+    elif command == "query":
+        pass
+    
     #
     # Return the current state of the board
     #
     nw0.send_reply(address, board)
+    #
+    # Send a notification to all subscribers that the board has changed
+    #
+    nw0.send_notification(notifications, command)
--- examples/boardgame/player.py
+++ examples/boardgame/player.py
@@ -1,18 +1,30 @@
 import networkzero as nw0
 
+def display_board(board):
+    print(board)
+
 address = nw0.discover("board")
+#
+# Send an query to get the state of the board before we begin
+#
+board = nw0.send_message(address, ["query"])
 
 player = input("Which player? ")
 #
-# Send an empty move to get the state of the board before we begin
-#
-board = nw0.send_message(address, [player, ""])
-#
-# Show the current state of the board, request a move, send the move
-# the board server and receive the new board state (including any moves
-# made by other players).
+# Show the current state of the board, send a move request to the board.
+# The board will return the new state of the board which we display.
+# Then wait until a move is made by someone else, show the new state
+# of the board and go round again.
 #
 while True:
-    print("Board:", board)
+    display_board(board)
+    
     move = input("Move: ")
-    board = nw0.send_message(address, [player, move])
+    board = nw0.send_message(address, ["move", player, move])
+    display_board(board)
+    
+    #
+    # Only interested in "move" updates, nothing else
+    #
+    nw0.wait_for_notification(address, "move")
+    board = nw0.send_message(address, ["query"])
