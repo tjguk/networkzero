5c62a0991d595d1e42ae6dcedf4a7ecfbf7097f0
Tim Golden <mail@timgolden.me.uk>
2016-04-06 16:49:20 +0100

    Add a little commentary

--- examples/chat/status.py
+++ examples/chat/status.py
@@ -15,11 +15,20 @@ while True:
         for name in new_people:
             print(name)
 
+    #
+    # Keep our internal list up to date
+    #
     people.update(all_people)
 
+    #
+    # Listen out for updates from each person in turn
+    #
     for person, address in people.items():
         topic, message = nw0.wait_for_notification(address, "chat", wait_for_s=0)
         if topic:
             print("%s says: %s" % (person, message))
     
+    #
+    # Pause so we don't hog the CPU
+    #
     time.sleep(1)
