a24d6a25cb7ee5101e8131a9719744f79b23c11b
Tim Golden <mail@timgolden.me.uk>
2016-04-11 20:07:56 +0100

    Send notification to the correct address

--- examples/quotes/quotes.py
+++ examples/quotes/quotes.py
@@ -19,15 +19,16 @@ def main(address_pattern=None):
 
     while True:
         services = [(name, address) for (name, address) in nw0.discover_all() if name != my_name]
-        
+
         for name, address in services:
             topic, message = nw0.wait_for_notification(address, "quote", wait_for_s=0)
             if topic:
                 print("%s says: %s" % (name, message))
-            quote = random.choice(quotes)
-            nw0.send_notification(address, "quote", quote)
         
-        time.sleep(0.5)
+        quote = random.choice(quotes)
+        nw0.send_notification(my_address, "quote", quote)
+        
+        time.sleep(1)
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
