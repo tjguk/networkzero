f99e56511025ed9ebccaacde79123930d7fe301b
Tim Golden <mail@timgolden.me.uk>
2016-04-13 22:55:04 +0100

    Halifax now works directly wih networkzero

--- examples/halifax/halifax.py
+++ examples/halifax/halifax.py
@@ -4,9 +4,6 @@ import time
 import traceback
 import uuid
 
-import zmq
-context = zmq.Context()
-
 import networkzero as nw0
 
 words = {}
@@ -14,7 +11,7 @@ with open("words.txt") as f:
     for word in f:
         words.setdefault(word[0], []).append(word.strip())
 
-my_name = uuid.uuid4().hex
+my_name = "halifax/" + uuid.uuid4().hex
 my_address = nw0.advertise(my_name)
 if len(sys.argv) == 2:
     first_word = sys.argv[1].lower().strip()
@@ -28,36 +25,22 @@ print("Waiting for neighbours to show up...")
 time.sleep(10)
 
 print("Looking for neighbours")
-addresses = [address for (name, address) in nw0.discover_all() if name != my_name]
+addresses = [address for (name, address) in nw0.discover_group("halifax") if name != my_name]
 print(addresses)
 
-listening_socket = context.socket(zmq.REP)
-listening_socket.bind("tcp://%s" % my_address)
-
-sending_socket = context.socket(zmq.REQ)
-for address in addresses:
-    sending_socket.connect("tcp://%s" % address)
-
-try:
-    while True:
-        
-        if first_word:
-            word = first_word
-            first_word = None
-        else:
-            print("Waiting for next word...")
-            word = listening_socket.recv().decode("utf-8")
-            listening_socket.send(word.encode("utf-8"))
-
-        print("Got word", word)
-        candidate_words = words[word[-1]]
-        random.shuffle(candidate_words)
-        next_word = candidate_words.pop()
-        
-        print("Sending word", next_word)
-        sending_socket.send(next_word.encode("utf-8"))
-        sending_socket.recv().decode("utf-8")
-except:
-    traceback.print_exc()
-    input("Press enter...")
+while True:
     
+    if first_word:
+        word = first_word
+        first_word = None
+    else:
+        print("Waiting for next word...")
+        word = nw0.wait_for_message(my_address, autoreply=True)
+
+    print("Got word", word)
+    candidate_words = words[word[-1]]
+    random.shuffle(candidate_words)
+    next_word = candidate_words.pop()
+    
+    print("Sending word", next_word)
+    nw0.send_message(addresses, next_word)
