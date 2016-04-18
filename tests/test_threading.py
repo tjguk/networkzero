c54ed2fea74b02c39e2965955f944b102e634b1f
Tim Golden <mail@timgolden.me.uk>
2016-04-18 18:00:45 +0100

    Ensure that the socket is created & bound in the support thread before attempting to rebind it in the test itself

--- tests/test_threading.py
+++ tests/test_threading.py
@@ -1,49 +1,49 @@
-import sys
-import contextlib
-import io
-import logging
 try:
     import queue
 except ImportError:
     import Queue as queue
-import re
 import threading
-import time
-import uuid
 
 import pytest
-import zmq
 
 import networkzero as nw0
 _logger = nw0.core.get_logger("networkzero.tests")
 nw0.core._enable_debug_logging()
 
-def support_test_bound_in_other_thread(address, event):
+def support_test_bound_in_other_thread(address, event1):
     #
-    # Create a socket in a thread and then pass it back to the test
-    # which can assert that it is not the same socket as one created
-    # within the test itself
+    # Create a socket in a thread and signal to the test
+    # which will try -- and fail -- to create a counterpart
+    # listening socket.
     #
     nw0.sockets.get_socket(address, "listener")
-    event.wait()   
+    event1.set()
 
 def test_bound_in_other_thread():
     """If a socket is bound in one thread it cannot be
     created and bound in another.
     """
-    event = threading.Event()
+    event1 = threading.Event()
     address = nw0.address()
-    t = threading.Thread(target=support_test_bound_in_other_thread, args=(address, event))
+    t = threading.Thread(target=support_test_bound_in_other_thread, args=(address, event1))
     t.setDaemon(True)
     t.start()
+    #
+    # Wait until the thread has created its socket otherwise
+    # we risk a race condition where the main thread creates
+    # first and the exception occurs in the support thread
+    #
+    event1.wait()
     with pytest.raises(nw0.SocketAlreadyExistsError):
         socket_from_this_thread = nw0.sockets.get_socket(address, "listener")
-    event.set()
     t.join()
 
-def support_test_connected_in_other_thread(address, q, event):
+def support_test_connected_in_other_thread(address, q):
+    #
+    # Create a socket in a thread which will be a different
+    # socket from its counterpart created in the test
+    #
     q.put(nw0.sockets.get_socket(address, "speaker"))
-    event.wait()
 
 def test_connected_in_other_thread():
     """If a sending socket is connected in a different thread, the socket returned
@@ -52,11 +52,10 @@ def test_connected_in_other_thread():
     event = threading.Event()
     q = queue.Queue()
     address = nw0.address()
-    t = threading.Thread(target=support_test_connected_in_other_thread, args=(address, q, event))
+    t = threading.Thread(target=support_test_connected_in_other_thread, args=(address, q))
     t.setDaemon(True)
     t.start()
     socket_from_other_thread = q.get()
     socket_from_this_thread = nw0.sockets.get_socket(address, "speaker")
     assert socket_from_other_thread is not socket_from_this_thread
-    event.set()
     t.join()
