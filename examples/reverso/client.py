a73c0918d2278a1374c0c1b97e71635aad462f53
Tim Golden <mail@timgolden.me.uk>
2016-05-05 08:50:58 +0100

    Use revised API

--- examples/reverso/client.py
+++ examples/reverso/client.py
@@ -1,8 +1,7 @@
 import networkzero as nw0
 
-address = nw0.discover("reverso")
+reverso = nw0.discover("reverso")
 while True:
     word = input("Enter word: ")
-    nw0.send_message_to(address, word)
-    reversed_word = nw0.wait_for_message_from(address)
+    reversed_word = nw0.send_message_to(reverso, word)
     print("Reversed:", reversed_word)
--- examples/reverso/server.py
+++ examples/reverso/server.py
@@ -3,4 +3,4 @@ import networkzero as nw0
 address = nw0.advertise("reverso")
 while True:
     message = nw0.wait_for_message_from(address)
-    nw0.send_message_to(address, message[::-1])
+    nw0.send_reply_to(address, message[::-1])
