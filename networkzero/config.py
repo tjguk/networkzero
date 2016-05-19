e39b59ab345d9d72a31d739218d68072d3794cf6
Tim Golden <mail@timgolden.me.uk>
2016-05-19 13:22:46 +0100

    Speed up the broadcast frequency

--- networkzero/config.py
+++ networkzero/config.py
@@ -11,14 +11,19 @@ COMMAND_ACK = "ack"
 #
 # Beacons will broadcast adverts at this frequency
 #
-BEACON_ADVERT_FREQUENCY_S = 5
+BEACON_ADVERT_FREQUENCY_S = 2
 #
 # Adverts will expire after this many seconds unless
 # a fresh broadcast is received. Default it above the
 # broadcast frequency so adverts are not forever expiring
 # and being recreated by the next received broadcast.
 #
-ADVERT_TTL_S = 3 * BEACON_ADVERT_FREQUENCY_S
+# NB since adverts are broadcast round-robin (ie only one advert
+# is broadcast every BEACON_ADVERT_FREQUENCY_S seconds) we need
+# to allow for the possibility that any given name might only
+# be advertised, say, once every 5 times.
+#
+ADVERT_TTL_S = 10 * BEACON_ADVERT_FREQUENCY_S
 
 VALID_PORTS = range(0x10000)
 DYNAMIC_PORTS = range(0xC000, 0x10000)
