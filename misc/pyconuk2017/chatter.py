4faf649ce0023a5b9b384fa89f3ef1f0dd5ab910
Tim Golden <mail@timgolden.me.uk>
2017-10-24 13:25:07 +0100

    First pass at slides

--- misc/pyconuk2017/chatter.py
+++ misc/pyconuk2017/chatter.py
@@ -9,16 +9,29 @@ box for descriptive text; a smaller box for inventory, strength & other
 attributes; and a line for entering commands
 """
 
+class FeedbackReader(QtCore.QThread):
+
+    message_received = QtCore.pyqtSignal(str, str)
+
+    def __init__(self, chattery):
+        super(FeedbackReader, self).__init__()
+        self.chattery = chattery
+
+    def run(self):
+        while True:
+            topic, message = nw0.wait_for_news_from(self.chattery)
+            if topic and message:
+                self.message_received.emit(topic, message)
+
 class Chatter(QtWidgets.QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, name, parent=None):
         super(Chatter, self).__init__(parent)
+        self.name = name
 
         self.text_panel = QtWidgets.QTextEdit()
         self.text_panel.setReadOnly(True)
-
         self.input = QtWidgets.QLineEdit()
-        self.input.editingFinished.connect(self.input_changed)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.text_panel, 3)
@@ -27,12 +40,31 @@ class Chatter(QtWidgets.QWidget):
         self.setLayout(layout)
         self.setWindowTitle("Chatter")
 
+        self.input.editingFinished.connect(self.input_changed)
+        self.input.setFocus()
+
+        self.chattery = nw0.discover("chattery/news")
+        self.responder = FeedbackReader(self.chattery)
+        self.responder.message_received.connect(self.handle_response)
+        self.responder.start()
+
+    def handle_response(self, topic, message):
+        text = self.text_panel.toPlainText()
+        if text:
+            text += "\n"
+        if topic == self.name:
+            topic = "ME"
+        text += "%s: %s" % (topic, message)
+        self.text_panel.setPlainText(text)
+
     def input_changed(self):
-        self.text_panel.setPlainText(self.text_panel.toPlainText() + "\n" + self.input.text())
+        text = self.input.text().strip()
+        if text:
+            nw0.send_news_to(self.chattery, self.name, text)
         self.input.clear()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    Chatter = Chatter()
+    Chatter = Chatter(sys.argv[1])
     Chatter.show()
     sys.exit(app.exec_())
